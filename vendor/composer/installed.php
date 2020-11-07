26aa47d569e5422b8c70c5d0ce89d1d76db4d505
abdullah ghanem <3bdullah.ghanem@gmail.com>
2020-11-07 02:52:09 +0200

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'db47c69e30469530d6c8fbe836f1c99d17f86819',
+    'name' => 'johntaa/ar-php',
+  ),
+  'versions' => 
+  array (
+    'johntaa/ar-php' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'db47c69e30469530d6c8fbe836f1c99d17f86819',
+    ),
+  ),
+);
